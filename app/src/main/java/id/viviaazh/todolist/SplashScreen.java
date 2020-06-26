f8daf8d1047bc76585b0e9f3c17d9589da1267ef
Rodhatul Vivia Firdaus Azhari <54561826+viviaazh@users.noreply.github.com>
2020-06-26 22:18:19 +0700

    splash screen, login xml, register xml

---
+++ app/src/main/java/id/viviaazh/todolist/SplashScreen.java
@@ -0,0 +1,31 @@
+package id.viviaazh.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(1200);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent (SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+
+                    finish();
+                }
+            }
+        };
+        thread.start();
+
+    }
+}
